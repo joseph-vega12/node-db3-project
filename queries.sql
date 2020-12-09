cd7c0aacd5899fcf7001f03fdf5326fd6f5782e2
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-09 13:52:15 -0800

    added some queries to queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,14 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-
+    select ProductName from Product
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+    select 
+        shipname, 
+        id, 
+        shippeddate
+    from [order]
+    where shippeddate < '2012-08-09'
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
