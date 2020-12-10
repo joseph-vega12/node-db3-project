66cc90aea2a6e2d18555fcfb22e66bbfb03f23e3
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-09 16:29:27 -0800

    added find findbyid and findsteps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,14 @@
-// scheme-model
+const db = require('../../data/db-config');
+
+module.exports = {
+    
+    find(){
+        return db('schemes');
+    },
+    findById(id){
+        return db('schemes').where({id}).first();
+    },
+    findSteps(id){
+        return db('steps').where({id})
+    }
+}
