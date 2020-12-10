f494499b992aa3fec714a5bf237c3d510e3263fd
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-09 21:18:30 -0800

    added steps model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,14 +1,18 @@
 const db = require('../../data/db-config');
 
 module.exports = {
-    
-    find(){
+
+    find() {
         return db('schemes');
     },
-    findById(id){
-        return db('schemes').where({id}).first();
+    findById(id) {
+        return db('schemes').where({ id }).first();
     },
-    findSteps(id){
-        return db('steps').where({id})
+    findSteps(id) {
+        return db('schemes as sc')
+            .join('steps as s', 's.scheme_id', 'sc.id')
+            .select('s.id', 's.step_number', 'sc.scheme_name', 's.instructions')
+            .where('s.scheme_id', id)
+            .orderBy('s.step_number');
     }
 }
